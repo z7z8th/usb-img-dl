7f9776d2a3a2bed635f4d1023a83822919f671bb
Bob.Zhao <bob.zhao@infomax.com.tw>
2012-06-28 22:45:46 -0400

    start to write erase functions

---
+++ usb_erase.py
@@ -0,0 +1,15 @@
+import os
+import io
+import sys
+from debug_util import *
+from util import *
+from const_vars import *
+from usb_generic import read_blocks, write_blocks, get_dev_block_info
+import struct
+
+
+def erase_dyn_id
+
+
+
+if __name__ == "__main__":
