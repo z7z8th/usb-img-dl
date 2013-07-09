f98c9d9dee6c704afca298daaf85cba948f09ad2
Bob.Zhao <bob.zhao@infomax.com.tw>
2013-07-09 10:32:26 +0800

    port_id_mapper.py ok

--- port_id_mapper.py
+++ port_id_mapper.py
@@ -1,23 +1,57 @@
+import threading
+import cPickle as pickle
 
 class port_id_mapper(object):
+    lock = threading.Lock()
     id_map = {}
-    
-    def __init__(self):
-        pass
+    default_file = "id_map.dict"
 
-    def update(self, port_id):
-        if port_id not in id_map:
-            pass
+    @staticmethod
+    def init():
+        port_id_mapper.load()
 
-    def load_from_file(self, file = None):
+    @staticmethod
+    def map_new(port_id):
+        assert(port_id not in port_id_mapper.id_map)
+        user_id = len(port_id_mapper.id_map) + 1
+        port_id_mapper.id_map[port_id] = user_id
+        port_id_mapper.dump()
+
+        
+    @staticmethod
+    def load(file = None):
+        print "(II) load port_id_mapper"
         if not file:
-            pass
+            file = port_id_mapper.default_file
+        with open(file, 'rb') as f:
+            port_id_mapper.id_map = pickle.load(f)
+
+            
+    @staticmethod
+    def dump(file = None):
+        if not file:
+            file = port_id_mapper.default_file
+        with open(file, 'wb') as f:
+            pickle.dump(port_id_mapper.id_map, f)
+
+            
+    @staticmethod
+    def get_user_id(port_id):
+        with port_id_mapper.lock:
+            if port_id not in port_id_mapper.id_map:
+                port_id_mapper.map_new(port_id)
+            return port_id_mapper.id_map[port_id]
+
+    @staticmethod
+    def dump_to_stdout():
+        print port_id_mapper.id_map
+
+port_id_mapper.load()
 
 
-    def save_to_file(self):
-        pass
-
-    def get_user_id(self, port_id):
-        if port_id not in id_map:
-            update()
-        return id_map[port_id]
+if __name__ == '__main__':
+    port_id_mapper.dump()
+    port_id_mapper.get_user_id('\x89\x11\x02\x90')
+    port_id_mapper.get_user_id('\x01\x03\x07')
+    port_id_mapper.dump_to_stdout()
+    port_id_mapper.dump()
